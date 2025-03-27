ed4440f2c9b6e2efb9c5357f07085a87ad5d47e9
Anwesha Singh <singhanwesha@gmail.com>
2025-03-27 19:02:57 +0530

    first-commit

--- home/urls.py
+++ home/urls.py
@@ -17,19 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
-
-
-# django admin changes
-admin.site.site_header = "Login to Burhan"
-admin.site.site_title = "Welocom to DashBord"
-admin.site.index_title = "Welocom to Portal"
-
-
-
 urlpatterns = [
     path('', views.home, name='home'),
-    path('project', views.project, name='project'),
-    path('#contact', views.contact, name='contact'),
     # path('skills', views.skills, name='skills'),
     # path('contact', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -15,18 +15,4 @@ def home(request):
     return render(request, 'home.html')
 
 
-def project(request):
-    return render(request, 'project.html')
-
-
-def contact(request):
-    #contact form database
-    if request.method == "POST":
-        name == request.POST['name']
-        email == request.POST['email']
-        subject == request.POST['subject']
-        message == request.POST['message']
-        contact = models.Home(name=name, email=email, subject=subject, message=message)
-        contact.save()
-    return render(request, 'home.html')
 
